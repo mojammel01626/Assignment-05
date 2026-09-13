01145b0ce6194179f0924c486fae2d6be8c6ff57
unknown <mojammel01626@gmail.com>
2026-09-13 15:58:24 +0600

    data.json added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Nav from './component/Nav';
 import Banner from './component/Banner';
+import Products from './component/Products';
+import type { IProductTypeDeclared } from './component/ProductDataTypes/Product';
+
+
+
+const productDataFetch = async ():Promise<IProductTypeDeclared[]> => {
+  const response = await fetch("/public/product.json");
+  const data = await response.json();
+  return data;
+}
+
 
 const App = () => {
+  
+const fetchData= productDataFetch();
+
+
   return (
     <div>
+
+
       <Nav/>
       <Banner/>
+
+      <Suspense fallback={<h2>Loadding.... </h2>}>
+          <Products fetchDataPushInProducts={fetchData}/>
+      </Suspense>
+
+
+
     </div>
   );
 };
---
+++ src/component/RightSideBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideBar = ({fetchDataProductsPushinRightSideBar}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RightSideBar;
