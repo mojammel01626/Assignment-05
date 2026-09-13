ee0e64a53f74b3f574bd4b3541c57413baa61807
unknown <mojammel01626@gmail.com>
2026-09-13 16:49:34 +0600

    Products services added

---
+++ src/component/Products.tsx
@@ -0,0 +1,41 @@
+import React, { use } from 'react';
+import type { IProductTypeDeclared } from './ProductDataTypes/Product';
+import LeftSideBar from './LeftSideBar';
+import RightSideBar from './RightSideBar';
+
+
+
+interface IProductsPropsType {
+    fetchDataPushInProducts: Promise<IProductTypeDeclared[]>
+}
+
+
+const Products = ({ fetchDataPushInProducts }: IProductsPropsType) => {
+    const fetchDataProducts = use(fetchDataPushInProducts)
+
+
+
+    return (
+        <div>
+            <div className='container mx-auto ]'>
+                <h1 className='font-black text-4xl'>Explore the <span className='text-blue-600'>Technologies</span></h1>
+                <p className='font-light text-xl text-gray-500 my-[20px]'>Pick one technology per category to build your ideal stack.</p>
+            </div>
+            
+            <div className=' container mx-auto grid grid-cols-12'>
+                <div className='bg-amber-500 col-span-9 '>
+                    
+                    <LeftSideBar fetchDataProductsPushinLeftSideBar={fetchDataProducts}/>
+                </div>
+
+
+                <div className=' bg-amber-950 col-span-3'>
+                        <RightSideBar fetchDataProductsPushinRightSideBar ={fetchDataProducts} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
+
